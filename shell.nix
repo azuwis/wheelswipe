ee60c376ab849dae7e3f261666feb5b70e7f4c38
Zhong Jianxin <azuwis@gmail.com>
2026-03-17 19:59:53 +0800

    Use agent-sandbox

--- shell.nix
+++ shell.nix
@@ -1,5 +1,48 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+{ }:
 
-pkgs.mkShell { packages = with pkgs; [ astyle ]; }
+let
+  sources = import ./sources.nix { };
+  pkgs = import sources.nixpkgs {
+    config.allowUnfreePredicate =
+      pkg:
+      builtins.elem (pkgs.lib.getName pkg) [
+        "claude-code"
+      ];
+  };
+  agent-sandbox = import sources.agent-sandbox.outPath {
+    inherit pkgs;
+  };
+  claude-sandboxed = agent-sandbox.mkSandbox {
+    pkg = pkgs.claude-code;
+    binName = "claude";
+    outName = "claude-sandboxed";
+    allowedPackages = [
+      pkgs.coreutils
+      pkgs.which
+      pkgs.bash
+      pkgs.git
+      pkgs.ripgrep
+      pkgs.fd
+      pkgs.gnused
+      pkgs.gnugrep
+      pkgs.findutils
+      pkgs.jq
+    ];
+    stateDirs = [ "$HOME/.claude" ];
+    stateFiles = [
+      "$HOME/.claude.json"
+      "$HOME/.claude.json.lock"
+    ];
+    extraEnv = {
+      ANTHROPIC_AUTH_TOKEN = "$ANTHROPIC_AUTH_TOKEN";
+      ANTHROPIC_BASE_URL = "$ANTHROPIC_BASE_URL";
+    };
+  };
+in
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    astyle
+    claude-sandboxed
+  ];
+}
