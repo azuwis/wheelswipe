c8051fc4acc50fbda6da854c932cb97010e481f6
Zhong Jianxin <azuwis@gmail.com>
2026-04-27 20:12:57 +0800

    Remove claude-sandboxed related from *.nix
    Use fence instead

--- shell.nix
+++ shell.nix
@@ -1,53 +1,5 @@
-{ }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
-let
-  sources = import ./sources.nix { };
-  pkgs = import sources.nixpkgs {
-    config.allowUnfreePredicate =
-      pkg:
-      builtins.elem (pkgs.lib.getName pkg) [
-        "claude-code"
-      ];
-  };
-  agent-sandbox = import sources.agent-sandbox.outPath {
-    inherit pkgs;
-  };
-  claude-sandboxed = agent-sandbox.mkSandbox {
-    pkg = pkgs.claude-code;
-    binName = "claude";
-    outName = "claude-sandboxed";
-    allowedPackages = [
-      pkgs.cacert
-      pkgs.coreutils
-      pkgs.which
-      pkgs.bash
-      pkgs.git
-      pkgs.ripgrep
-      pkgs.fd
-      pkgs.gnused
-      pkgs.gnugrep
-      pkgs.findutils
-      pkgs.jq
-    ];
-    stateDirs = [ "$HOME/.claude" ];
-    stateFiles = [
-      "$HOME/.claude.json"
-      "$HOME/.claude.json.lock"
-    ];
-    extraEnv = {
-      ANTHROPIC_AUTH_TOKEN = "$ANTHROPIC_AUTH_TOKEN";
-      ANTHROPIC_BASE_URL = "$ANTHROPIC_BASE_URL";
-      NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-      SSL_CERT_DIR = "${pkgs.cacert}/etc/ssl/certs";
-      SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-    };
-    restrictClosure = true;
-  };
-in
-
-pkgs.mkShell {
-  packages = with pkgs; [
-    astyle
-    claude-sandboxed
-  ];
-}
+pkgs.mkShell { packages = with pkgs; [ astyle ]; }
